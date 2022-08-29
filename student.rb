5ee4968d48bf51a1670f8ef8499bd65185dccd20
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-29 23:43:38 +0100

    Add super constructor

--- student.rb
+++ student.rb
@@ -1,7 +1,9 @@
-require "./person.rb"
+require_relative "person"
 
 class Student < Person
+
   def initialize(classroom)
+    super(name = "unknown", age, parent_permission = true)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,9 @@
-require "./person.rb"
+require_relative "person"
 
 class Teacher < Person
   
   def initialize(@specialization)
+    super(name = "unknown", age, parent_permission = true)
     @specialization = specialization
   end
 
