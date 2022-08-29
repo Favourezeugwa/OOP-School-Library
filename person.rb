60e93e77fdbbbacb027fc6ad7d13d751bfaf5104
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-29 23:29:00 +0100

    Add Person class

---
+++ person.rb
@@ -0,0 +1,27 @@
+class Person
+  def initialize(name = "unknown", age, parent_permission = true)
+    @id = Randon.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader : id
+  attr_accessor : name
+  attr_accessor : age
+
+  def is_of_age?
+    if @age >= 18
+      true
+    elsif @age < 18
+      false
+    end
+  end
+
+  def can_use_services?
+    if @age >= 18 || @parent_permission = true
+      true
+    end
+  end
+
+end
