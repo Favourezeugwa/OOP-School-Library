ddc199866cbe44d1653c9f60f1a266a0ffdbff76
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-30 22:44:21 +0100

    Add base decorator and nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Randon.rand(1..1000)
     @name = name
@@ -23,5 +25,9 @@ class Person
 
   def can_use_services?
     return unless @age >= 18 || @parent_permission == true
+  end
+
+  def correct_name
+    @correct_name = name
   end
 end
