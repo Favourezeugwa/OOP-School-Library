ddc0c312a5736159de0a06056bc7f5d6542619ba
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-08 10:28:49 +0100

    Add nameable_spec test

--- spec/base_decor_spec.rb
+++ spec/base_decor_spec.rb
@@ -2,10 +2,17 @@ require_relative 'spec_helper'
 
 describe BaseDecorator do
   let(:base1) { BaseDecorator.new('Favour') }
+  let(:nameable) { Nameable.new }
 
   describe '#initialize' do
     it 'returns a BaseDecor object' do
-      expect(decor).to be_an_instance_of(BaseDecor)
+      expect(base1).to be_an_instance_of(BaseDecorator)
     end
   end
+
+  # describe '#correct_name' do
+  #   it 'returns the correct name' do
+  #     expect(base1.nameable.correct_name).to eql 'Favour'
+  #   end
+  # end
 end
